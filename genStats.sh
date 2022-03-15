5135a12bde5a4db96f3b9371a8ffe76774db71a1
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2022-03-15 11:26:20 +0000

    quote docker volume paths to avoid potential trouble

--- genStats.sh
+++ genStats.sh
@@ -3,7 +3,6 @@
 conf_dir=$(dirname "$(readlink -f "$0")")
 parent_dir="$(dirname "$conf_dir")"
 logs_dir="$parent_dir/working_logs"
-sss_dir="$parent_dir/superseriousstats"
 website_dir="$(dirname "$parent_dir")/www"
 
 source_log="$1"
@@ -20,7 +19,7 @@ echo "### Splitting single .weechatlog into daily logs..."
 python3 "$conf_dir/splitLogs.py" -f "$source_log" -p "$logs_dir/newserver.desertbus." -s .weechatlog -c
 
 echo "### Importing daily logs and generating stats page..."
-docker run -it -v $conf_dir/nick_aliases.txt:/tmp/nick_aliases.txt -v $conf_dir/desertbus.db3:/tmp/db/desertbus.db3 -v $logs_dir:/tmp/logs -v $website_dir:/var/www/html/sss --rm chat-stats
+docker run -it -v "$conf_dir/nick_aliases.txt:/tmp/nick_aliases.txt" -v "$conf_dir/desertbus.db3:/tmp/db/desertbus.db3" -v "$logs_dir:/tmp/logs" -v "$website_dir:/var/www/html/sss" --rm chat-stats
 
 rm "$conf_dir/stats-gen.lock"
 
